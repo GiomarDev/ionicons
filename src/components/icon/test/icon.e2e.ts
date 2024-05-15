90603c46201c0ec2f39386275ab22d70f7caf04b
Giomar <65304193+GiomarDev@users.noreply.github.com>
2024-05-15 12:57:21 -0500

    Update icon.e2e.ts

--- src/components/icon/test/icon.e2e.ts
+++ src/components/icon/test/icon.e2e.ts
@@ -10,21 +10,9 @@ test.describe('icon: basic', () => {
 
     await expect(page).toHaveScreenshot(`icon-diff.png`, { fullPage: true });
   });
-
   test('some icons should flip when rtl', async ({ page }) => {
     await page.goto(`/`);
-
-    const autoflip = page.locator('.auto-flip-chevrons [name=chevron-forward] .icon-inner');
-    const unflip = page.locator('.un-flip-chevrons [name=chevron-forward] .icon-inner');
-    await expect(autoflip).not.toHaveCSS('transform', /matrix\(-1/);
-    await expect(unflip).not.toHaveCSS('transform', /matrix\(-1/);
-
-    await page.evaluate(() => {
-      document.dir = 'rtl';
     });
-
-    await expect(autoflip).toHaveCSS('transform', /matrix\(-1/);
-    await expect(unflip).not.toHaveCSS('transform', /matrix\(-1/);
 
     // Wait for all SVGs to be lazily loaded before taking screenshots
     await page.waitForLoadState('networkidle');
